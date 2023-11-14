3d379a413bc4b99a90d7047505c3e73e72f0d9d6
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-11-14 03:07:49 -0500

    scss new styling and nesting

--- my-new-app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import renderer from 'react-test-renderer'
-
-test('renders a snapshot', () => {
-  const tree = renderer.create(<App/>).toJSON()
-  expect(tree).toMatchSnapshot()
-})
--- my-new-app/src/tests/instructions.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import Instructions from './instructions';
-
-test('renders instructions component snapshot', () => {
-  const { asFragment } = render(<Instructions />);
-  expect(asFragment()).toMatchSnapshot();
-});
-
